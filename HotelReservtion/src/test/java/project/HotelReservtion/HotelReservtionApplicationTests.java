876b8f4bda3c2be5f0337083a40531771148df9f
gitkashyap <33050257+gitkashyap@users.noreply.github.com>
2021-03-16 13:17:19 +0530

    loading file structure for project

---
+++ HotelReservtion/src/test/java/project/HotelReservtion/HotelReservtionApplicationTests.java
@@ -0,0 +1,13 @@
+package project.HotelReservtion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservtionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

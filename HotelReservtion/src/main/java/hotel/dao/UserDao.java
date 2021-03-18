43864aa2595b11e62ccca9293197b323acaf3dd7
gitkashyap <33050257+gitkashyap@users.noreply.github.com>
2021-03-19 00:31:04 +0530

    almost done

---
+++ HotelReservtion/src/main/java/hotel/dao/UserDao.java
@@ -0,0 +1,14 @@
+package hotel.dao;
+
+import hotel.entity.User;
+
+//DAO Pattern for User
+public interface UserDao {
+
+    public User findUserByEmail(String email);
+
+    public User findUserByUsername(String username);
+    
+    public void saveUser(User user);
+        
+}

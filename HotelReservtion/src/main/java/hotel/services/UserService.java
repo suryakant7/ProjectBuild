01fffa274e6115dd208c9ba22c351bebbb18261c
LENOVO <LENOVO@DESKTOP-CRQU31L>
2021-03-19 11:47:42 +0530

    Commited

---
+++ HotelReservtion/src/main/java/hotel/services/UserService.java
@@ -0,0 +1,17 @@
+package hotel.services;
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import hotel.entity.User;
+import hotel.temp.CurrentUser;
+
+//Service Pattern for User
+public interface UserService extends UserDetailsService {
+
+	public User findUserByEmail(String email);
+
+	public void saveUser(CurrentUser currentUser);
+
+	public int getLoggedUserId();
+}
